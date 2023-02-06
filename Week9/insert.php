b92d2c79436b89588aefecff758efe1c604e5555
JjArchog <acpojol@student.apc.edu.ph>
2023-02-06 14:08:33 +0800

    finished php code & html form

--- Week9/insert.php
+++ Week9/insert.php
@@ -1,9 +1,55 @@
 <?php
+$fullName = filter_input(INPUT_POST, 'fullName');
+$emailUsed = filter_input(INPUT_POST, 'emailUsed');
+$subjectUsed = filter_input(INPUT_POST, 'subjectUsed');
+$messageInput = filter_input(INPUT_POST, 'messageInput');
 
-if (isset($_POST['name']) && isset ($_POST['meesage'])){
-    include 'db_conn.php';
+if (!empty($fullName)){
+    if (!empty($emailUsed)){
+        if(!empty($subjectUsed)){
+            if(!empty($messageInput)){
 
-    echo "Hello";
+                $host = "localhost";
+                $dbusername = "root";
+                $dbpassword = "";
+                $dbname = "test_db";
+
+// CONNECTION HERE
+$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+    die('Connect Error (' . mysqli_connect_errno() . ') '
+    . mysqli_connect_error());
 }else{
-    header("Location: index.html");
-}
+    $sql = "INSERT INTO inquiry (fullName, emailUsed, subjectUsed, messageInput)
+    values ('$fullName', '$emailUsed', '$subjectUsed', '$messageInput')";
+    if ($conn->query($sql)){
+        echo "New Record has been added.";
+    }else{
+        echo "Error: ". $sql . "<br>". $conn->error;
+    } 
+    $conn->close();
+    }
+
+            
+            } else {
+                echo "Message should not be empty, Try Again.";
+                die ();
+            }
+        } else {
+            echo "Subject should not be empty, Try Again.";
+            die ();
+        }
+    } else {
+        echo "Email should not be empty, Try Again.";
+        die();
+    } 
+} else {
+        echo "Name is required, Try Again.";
+        die();
+    }
+
+
+   
+
+?>
